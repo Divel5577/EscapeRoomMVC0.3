6a795c3b0d6ac74f8d2f02bedf6e84529ef29233
Divel5577 <gitpdulkowski@gmail.com>
2024-11-12 15:50:48 +0100

    updt

--- EscapeRoomMVC0.3/obj/Debug/net8.0/EscapeRoomMVC0.3.AssemblyInfo.cs
+++ EscapeRoomMVC0.3/obj/Debug/net8.0/EscapeRoomMVC0.3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EscapeRoomMVC0.3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18834865a808f14cddf294e6bed113ceec4d8cdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EscapeRoomMVC0.3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EscapeRoomMVC0.3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
